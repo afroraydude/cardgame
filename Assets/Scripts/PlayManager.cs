fd62d54716fa83e000eddd8f193a793d7ad9bb67
afroraydude <afroraydude@protonmail.com>
2021-01-01 21:26:45 -0500

    initial commit

---
+++ Assets/Scripts/PlayManager.cs
@@ -0,0 +1,113 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PlayManager : MonoBehaviour
+    {
+        public enum Actions
+        {
+            Heavy = 0,
+            Shield = 1,
+            Sword = 2,
+        }
+        
+        [SerializeField]
+        public int[] actionSet = {-1,-1,-1,-1,-1};
+
+        void Start()
+        {
+        }
+        
+        public int energyPoints = 7;
+
+        public bool InsertAction(int spot, int action)
+        {
+            if (spot == 4 && action == 0)
+            {
+                Debug.Log($"err1 {spot} {action}");
+                return false;
+            }
+
+            if (spot - 1 >= 0 && (actionSet[spot - 1]) == 0)
+            {
+                Debug.Log($"err2 {spot} {action}");
+                if (action == 0)
+                {
+                    actionSet[spot + 1] = -1;
+                }
+                return false;
+            }
+
+            if (!CheckEnergy(spot, action))
+            {
+                if (action == 0)
+                {
+                    actionSet[spot + 1] = -1;
+                }
+                return false;
+            }
+            
+            actionSet[spot] = action;
+            return true;
+        }
+
+        public bool CheckEnergy(int spot, int action)
+        {
+            int energy = energyPoints;
+            // if spot was previously filled in
+            if (actionSet[spot] != -1)
+            {
+                // restore points
+                switch (actionSet[spot])
+                {
+                    case 0:
+                        energy += 0;
+                        break;
+                    case 1:
+                        energy += 1;
+                        break;
+                    case 2:
+                        energy += 2;
+                        break;
+                }
+            }
+            
+            switch (action)
+            {
+                case 0:
+                    energy -= 0;
+                    break;
+                case 1:
+                    energy -= 1;
+                    break;
+                case 2:
+                    energy -= 2;
+                    break;
+            }
+            if (energy < 0) return false;
+            return true;
+        }
+        
+        public void CalculateEnergy()
+        {
+            int energy = 7;
+            for (int i = 0; i <= 4; i++)
+            {
+                int actionType = actionSet[i];
+                switch (actionType)
+                {
+                    case 0:
+                        energy -= 0;
+                        break;
+                    case 1:
+                        energy -= 1;
+                        break;
+                    case 2:
+                        energy -= 2;
+                        break;
+                }
+            }
+            this.energyPoints = energy;
+        }
+    }
+}
---
+++ Assets/Scripts/UpdateUI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DefaultNamespace
+{
+
+    public class UpdateUI : MonoBehaviour
+    {
+        [SerializeField] private Text energyPoints;
+        [SerializeField] private Text[] actionTexts;
+        [SerializeField] public PlayManager playManager;
+
+        private void Start()
+        {
+            
+        }
+
+        public void UpdateTextElements()
+        {
+            for (int i = 0; i <= 4; i++)
+            {
+                int actionType = playManager.actionSet[i];
+
+                switch (actionType)
+                {
+                    case 0:
+                        actionTexts[i].text = "Heavy";
+                        break;
+                    case 1:
+                        actionTexts[i].text = "Shield";
+                        break;
+                    case 2:
+                        actionTexts[i].text = "Sword";
+                        break;
+                }
+            }
+
+            energyPoints.text = playManager.energyPoints.ToString();
+        }
+    }
+}
